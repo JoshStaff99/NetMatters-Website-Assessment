be82f902b446d2b3e33e405fdc4412cc1d87ae51
JoshStaff99 <Joshua.Stafford@netmatters-scs.com>
2024-12-12 13:35:05 +0000

    Sticky header
    Added the sticky header to the page. Works in all viewports and browsers with correct styling applied

---
+++ js/main.js
@@ -0,0 +1,40 @@
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('.fake-header').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('.fake-header').removeClass('nav-down').addClass('nav-up');
+        $('.header-placeholder').removeClass('header-show').addClass('header-hidden');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('.fake-header').removeClass('nav-up').addClass('nav-down');
+            $('.header-placeholder').removeClass('header-hidden').addClass('header-show');
+        }
+    }
+    
+    lastScrollTop = st;
+}
